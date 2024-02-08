54adee5f7282b37d046a5bafc8dc3eeff8d9c026
David <david.roebl@yahool.com>
2024-02-08 21:06:40 +0100

    add script for model development scikit learn

--- config/config.py
+++ config/config.py
@@ -6,16 +6,16 @@ GEOSPHERE_INCA_TS_URL = 'https://dataset.api.hub.geosphere.at/v1/timeseries/hist
 PROJECT_EPSG = "EPSG:31287"
 
 # Path to project directory
-# BASE_PATH = "C:/Users/David/Documents/ZGIS/Nextcloud_MyFiles/Projects/IGNITE"
-BASE_PATH = "C:/Users/b1105474/OneDrive - Universität Salzburg/PR_IGNITE/IGNITE_david"
+BASE_PATH = "C:/Users/David/Documents/ZGIS/One_Drive_Uni_Salzburg/PR_IGNITE/IGNITE_david"
+# BASE_PATH = "C:/Users/b1105474/OneDrive - Universität Salzburg/PR_IGNITE/IGNITE_david"
 
 # Path to yaml file where paths are specified
-# PATH_TO_PATH_CONFIG_FILE = "C:/Users/David/Documents/ZGIS/Gitlab_Projects/ignite-wildfire-ignition-prediction/config/paths.yaml"
-PATH_TO_PATH_CONFIG_FILE = "C:/Users/b1105474/OneDrive - Universität Salzburg/Gitlab_Projects//ignite-wildfire-ignition-prediction/config/paths.yaml"
+PATH_TO_PATH_CONFIG_FILE = "C:/Users/David/Documents/ZGIS/One_Drive_Uni_Salzburg/Gitlab_Projects//ignite-wildfire-ignition-prediction/config/paths.yaml"
+# PATH_TO_PATH_CONFIG_FILE = "C:/Users/b1105474/OneDrive - Universität Salzburg/Gitlab_Projects//ignite-wildfire-ignition-prediction/config/paths.yaml"
 
 # TODO exchange with real BBOX (currently this is BBOX of upper Austria)
 # Bounding Box for Austria (e.g. used for ffmc layer creation)
 BBOX_AUSTRIA = [47.421389, 12.73, 48.776944, 15.036111]
 
 # activate conda env
-# C:\Users\b1105474\AppData\Local\miniconda3\condabin\activate.bat pymc_env   
+# C:\Users\b1105474\AppData\Local\miniconda3\condabin\activate.bat pymc_env
