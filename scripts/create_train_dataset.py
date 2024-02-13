9bdc4453eb08b8242fcdf6bd8a2e4ea12e343f04
Röbl David <david.roebl@plus.ac.at>
2024-02-13 14:38:18 +0100

    rearrange project; update create_training_data script

--- scripts/create_train_dataset.py
+++ scripts/create_train_dataset.py
@@ -4,20 +4,13 @@ utilizing the static feature layers and the ffmc.
 '''
 
 import os
-import re
-import json
 import numpy as np
 import rasterio
-import shapely
 import pandas as pd
 import geopandas as gpd
 
 from config.config import BASE_PATH, PATH_TO_PATH_CONFIG_FILE
 from src.utils import load_paths_from_yaml, replace_base_path
-from src.data_preprocessing.inca_data_preprocessing import calculate_wind_speed, calculate_ffmc
-
-
-INITIAL_FFMC_VALUE = 85
 
 
 def add_static_feature_from_raster(events: gpd.GeoDataFrame,
@@ -30,18 +23,19 @@ def add_static_feature_from_raster(events: gpd.GeoDataFrame,
         events (gpd.GeoDataFrame): GeoDataFrame containing the date and location of the fire and non-fire events
 
     Returns:
-        gpd.GeoDataFrame: fire and non-fire events with new column for feature values
+        pd.DataFrame: fire and non-fire events with new column for feature values
     """
 
     with rasterio.open(path_to_raster) as src:
-        events_updated = events.copy()
         coords = list(zip(events.geometry.x, events.geometry.y))
         events_updated = events.copy()
         events_updated[feature_name] = [x[0] for x in src.sample(coords)]
 
         events_updated.loc[events_updated[feature_name] ==
                            src.profile["nodata"], feature_name] = np.nan
-    return events_updated
+
+    return gpd.GeoDataFrame(
+        events_updated, geometry="geometry", crs="EPSG:31287")
 
 
 def get_nearest_pop_value(row) -> float:
@@ -59,11 +53,11 @@ def get_nearest_pop_value(row) -> float:
     return row[pop_col_name]
 
 
-def add_static_features(event_data: gpd.GeoDataFrame, feature_info: dict) -> gpd.GeoDataFrame:
+def add_static_features(event_data: gpd.GeoDataFrame, feature_info: list) -> gpd.GeoDataFrame:
     """adds static features from rasters to dataframe. 
 
     Args:
-        feature_info (dict): Column names and paths to rasters are defined in feature_info dict
+        feature_info (list): Column names and paths to rasters are defined in feature_info list
 
     Returns:
         gpd.GeoDataFrame: dataframe with labels and static features
@@ -83,59 +77,7 @@ def add_static_features(event_data: gpd.GeoDataFrame, feature_info: dict) -> gpd
     return event_data
 
 
-def convert_inca_data_from_json_to_dataframe(path_to_inca_data: str) -> gpd.GeoDataFrame:
-    """Convert the raw inca data, retrieved from Geosphere Data API to GeoDataFrame. 
-    For rainfall the sum of full time range is calculated. For the other parameters only the last value is taken. 
-
-    Args:
-        path_to_inca_data (str): Path to raw inca data
-
-    Returns:
-        gpd.GeoDataFrame: GeoDataFrame with epsg:4326
-    """
-
-    data_list = []
-
-    with open(path_to_inca_data, 'r') as file:
-        for line in file:
-            # Use regular expression to extract ID and JSON content
-            match = re.match(r'(\d+)(\{.+})', line)
-            if match:
-                numeric_id = int(match.group(1))
-                json_data = json.loads(match.group(2))
-
-                last_timestamp = json_data["timestamps"][-1]
-                last_T2M = json_data["features"][0]["properties"]["parameters"]["T2M"]["data"][-1]
-                last_RH2M = json_data["features"][0]["properties"]["parameters"]["RH2M"]["data"][-1]
-                last_UU = json_data["features"][0]["properties"]["parameters"]["UU"]["data"][-1]
-                last_VV = json_data["features"][0]["properties"]["parameters"]["VV"]["data"][-1]
-
-                # Chechking for None values here is neccessary - because None's appear in RR
-                rr_data = json_data["features"][0]["properties"]["parameters"]["RR"]["data"]
-                if any(val is None for val in rr_data):
-                    sum_RR = None
-                else:
-                    sum_RR = np.sum(rr_data)
-
-                coordinates = json_data["features"][0]["geometry"]["coordinates"]
-                point = shapely.Point(coordinates)
-
-                data_list.append({
-                    "ID": numeric_id,
-                    "Timestamp": last_timestamp,
-                    "T2M": last_T2M,
-                    "RH2M": last_RH2M,
-                    "UU": last_UU,
-                    "VV": last_VV,
-                    "RR_sum_24h": sum_RR,
-                    "geometry": point
-                })
-
-    gdf = gpd.GeoDataFrame(data_list, geometry='geometry', crs="EPSG:4326")
-    return gdf
-
-
-def add_ffmc_feature(event_data: gpd.GeoDataFrame, path_to_inca_data: str) -> gpd.GeoDataFrame:
+def add_ffmc_feature(event_data: gpd.GeoDataFrame, path_to_ffmc_data: str) -> gpd.GeoDataFrame:
     """add column named ffmc to training dataframe
 
     Args:
@@ -146,18 +88,11 @@ def add_ffmc_feature(event_data: gpd.GeoDataFrame, path_to_inca_data: str) -> gp
         (gpd.GeoDataFrame): event_data with additional column called ffmc, with epsg=31287
     """
 
-    inca_data_gdf = convert_inca_data_from_json_to_dataframe(path_to_inca_data)
-    inca_data_gdf.dropna(inplace=True)
-
-    inca_data_gdf["windspeed"] = inca_data_gdf.apply(
-        lambda row: calculate_wind_speed(row["UU"], row["VV"]), axis=1)
-    inca_data_gdf["ffmc"] = inca_data_gdf.apply(lambda row:  calculate_ffmc(
-        INITIAL_FFMC_VALUE, row["RH2M"], row["T2M"], row["RR_sum_24h"], row["windspeed"]), axis=1)
-
-    train_data = pd.merge(event_data, inca_data_gdf.loc[:, ["ID", "ffmc"]],
-                          left_on="index", right_on="ID")
-    return gpd.GeoDataFrame(
-        train_data, geometry="geometry", crs="EPSG:31287")
+    ffmc_df = pd.read_csv(path_to_ffmc_data)
+    ffmc_event_df = pd.merge(event_data, ffmc_df.loc[:, [
+        "X", "ffmc"]], left_on="index", right_on="X")
+    ffmc_event_df.drop(columns=["X"], inplace=True)
+    return ffmc_event_df
 
 
 def main():
@@ -184,8 +119,8 @@ def main():
     ]
 
     train_data = add_static_features(event_data, feature_info)
-    train_data = add_ffmc_feature(train_data, paths["inca"]["training_data"])
-    train_data.to_file(paths["training_data"]["subset"])
+    train_data = add_ffmc_feature(train_data, paths["ffmc_events"]["source"])
+    train_data.to_file(paths["training_data"])
 
 
 if __name__ == "__main__":
