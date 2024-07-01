b97a2010fa353645fe7d59add268a9eb16bde65f
David Röbl <davidsam.roebl@gmail.com>
2024-07-01 15:51:11 +0200

    finished results of uncertainty papaer

--- src/modeling/predictions.py
+++ src/modeling/predictions.py
@@ -39,68 +39,70 @@ class BinaryClassification(BayesianPrediction):
         seed: int,
         y_var_name: str,
         p_var_name: str,
+        z_var_name: str,
     ):
-        super().__init__(model, trace, x_new, [y_var_name, p_var_name], seed)
+        super().__init__(
+            model, trace, x_new, [y_var_name, p_var_name, z_var_name], seed
+        )
         self.y_var_name = y_var_name
         self.p_var_name = p_var_name
+        self.z_var_name = z_var_name
 
     def get_predictions(self, threshold: float = 0.5):
         """return binary predictions based on the threshold"""
 
-        p_pred = self.trace_pred.posterior_predictive[self.y_var_name].mean(
+        z_pred = self.trace_pred.posterior_predictive[self.z_var_name].mean(
+            dim=["chain", "draw"]
+        )
+        p_pred = self.trace_pred.posterior_predictive[self.p_var_name].mean(
             dim=["chain", "draw"]
         )
         y_pred = (p_pred >= threshold).astype("int")
-        return y_pred, p_pred
+        return y_pred, p_pred, z_pred
 
-    def get_binary_entropy(self):
-        """function calculates binary entropy"""
-
-        arr = self.trace_pred.posterior_predictive[self.y_var_name]
-
-        def binary_entropy(posterior: np.array):
-            n1 = np.sum(posterior)
-            n = len(posterior)
-            p1 = n1 / n
-            p0 = 1 - p1
-            jitter = 1e-8  # small value to avoid taking log of 0
-            entropy = -(p1 * np.log2(p1 + jitter) + p0 * np.log2(p0 + jitter))
-            return entropy
-
-        # TODO check if everything went right; changed from (2, 1) to 0
-        entropy_all_traces = np.apply_along_axis(binary_entropy, axis=0, arr=arr)
-        return np.mean(entropy_all_traces, axis=0)
-
-    def get_hdi(self, prob: float = 0.95):
+    def get_hdi(self, var: str, prob: float):
         """
         function calculates high density interval (interval with the biggest distribution mass) of predictions
         """
 
-        hdi = az.hdi(
-            self.trace_pred.posterior_predictive[self.p_var_name], hdi_prob=prob
-        )[self.p_var_name].values
+        var_value = getattr(self, var)
+
+        hdi = az.hdi(self.trace_pred.posterior_predictive[var_value], hdi_prob=prob)[
+            var_value
+        ].values
         hdi_width = hdi[:, 1] - hdi[:, 0]
         return hdi, hdi_width
 
-    def predict(self, pred_threshold: float = 0.5, hdi_prob: float = 0.8):
+    def predict(
+        self,
+        pred_threshold: float = 0.5,
+        hdi_prob: float = 0.95,
+        include_z: bool = True,
+    ):
         """
         combines y and p predictions, hdi and binary entropy into one dataframe
         """
 
         df = pd.DataFrame()
-        y_pred, p_pred = self.get_predictions(pred_threshold)
-        hdi, hdi_width = self.get_hdi(hdi_prob)
-        binary_entropy = self.get_binary_entropy()
+        y_pred, p_pred, z_pred = self.get_predictions(pred_threshold)
+        p_hdi, p_hdi_width = self.get_hdi("p_var_name", hdi_prob)
         df["y_pred"] = y_pred
         df["p_pred"] = p_pred
-        df["hdi_lower"] = hdi[:, 0]
-        df["hdi_upper"] = hdi[:, 1]
-        df["hdi_width"] = hdi_width
-        df["binary_entropy"] = binary_entropy
+        df["z_pred"] = z_pred
+        df["p_hdi_lower"] = p_hdi[:, 0]
+        df["p_hdi_upper"] = p_hdi[:, 1]
+        df["p_hdi_width"] = p_hdi_width
+
+        if include_z:
+            z_hdi, z_hdi_width = self.get_hdi("z_var_name", hdi_prob)
+            df["z_hdi_lower"] = z_hdi[:, 0]
+            df["z_hdi_upper"] = z_hdi[:, 1]
+            df["z_hdi_width"] = z_hdi_width
+
         return df
 
 
-class BinaryClassificationBNN(BinaryClassification):
+class BinaryClassificationBNN:
     def __init__(
         self,
         model: object,
@@ -110,7 +112,14 @@ class BinaryClassificationBNN(BinaryClassification):
         y_var_name: str,
         p_var_name: str,
     ):
-        super().__init__(model, trace, x_new, seed, y_var_name, p_var_name)
+        self.seed = seed
+        self.model = model
+        self.trace = trace
+        self.x_new = x_new
+        self.y_var_name = y_var_name
+        self.p_var_name = p_var_name
+        self.trace_pred = None
+        self.var_names_pred = [y_var_name, p_var_name]
 
     def extend_trace(self):
         """
@@ -127,3 +136,45 @@ class BinaryClassificationBNN(BinaryClassification):
             trace_new.extend(ppc)
 
         self.trace_pred = trace_new
+
+    def get_predictions(self, threshold: float = 0.5):
+        """return binary predictions based on the threshold"""
+
+        p_pred = self.trace_pred.posterior_predictive[self.p_var_name].mean(
+            dim=["chain", "draw"]
+        )
+        y_pred = (p_pred >= threshold).astype("int")
+        return y_pred, p_pred
+
+    def get_hdi(self, var: str, prob: float):
+        """
+        function calculates high density interval (interval with the biggest distribution mass) of predictions
+        """
+
+        var_value = getattr(self, var)
+
+        hdi = az.hdi(self.trace_pred.posterior_predictive[var_value], hdi_prob=prob)[
+            var_value
+        ].values
+        hdi_width = hdi[:, 1] - hdi[:, 0]
+        return hdi, hdi_width
+
+    def predict(
+        self,
+        pred_threshold: float = 0.5,
+        hdi_prob: float = 0.95,
+    ):
+        """
+        combines y and p predictions, hdi and binary entropy into one dataframe
+        """
+
+        df = pd.DataFrame()
+        y_pred, p_pred = self.get_predictions(pred_threshold)
+        p_hdi, p_hdi_width = self.get_hdi("p_var_name", hdi_prob)
+        df["y_pred"] = y_pred
+        df["p_pred"] = p_pred
+        df["p_hdi_lower"] = p_hdi[:, 0]
+        df["p_hdi_upper"] = p_hdi[:, 1]
+        df["p_hdi_width"] = p_hdi_width
+
+        return df
